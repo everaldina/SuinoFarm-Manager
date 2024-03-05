d1db1f3b966a1adaf5af4f335c9a91c2358044eb
Caua-Clemente <cauaclementelima@gmail.com>
2024-03-05 16:36:40 -0300

    Criando componente de cadastro de suino e importanto modulos

--- src/app/components/form-cadastro/form-cadastro.component.ts
+++ src/app/components/form-cadastro/form-cadastro.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-cadastro',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class FormCadastroComponent {
 
+  formularioSuino: FormGroup;
+  listaStatus: string[] = ['Ativo', 'Vendido', 'Morto'];
+  listaSexos: string[] = ['M', 'F'];
+
+  constructor(){
+    this.formularioSuino = new FormGroup({
+      'brincoAnimal': new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[0-9]*$/)
+      ]),
+
+      'brincoPai': new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[0-9]*$/)
+      ]),
+
+      'brincoMae': new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[0-9]*$/)          
+      ]),
+
+      'dataNascimento': new FormControl(null, [
+          Validators.required
+      ]),
+
+      'dataSaida': new FormControl(null, [
+          Validators.required
+      ]),
+
+      'status': new FormControl(null, [
+          Validators.required
+      ]),
+
+      'sexo': new FormControl(null, [
+          Validators.required
+      ]),
+    })
+  }
+
+  onSubmit(){
+    if(this.formularioSuino.valid)
+      console.log("Formulário válido", this.formularioSuino.value);
+    else
+      console.log("Formulário inválido")
+  }
 }
