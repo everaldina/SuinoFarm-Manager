faa520e4e833a1ba4ee9c9be6d1b532eec976e7b
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-03-07 16:09:49 -0300

    att classe suino

--- src/app/suino.ts
+++ src/app/suino.ts
@@ -1,2 +1,9 @@
 export interface Suino {
+    brinco: string;
+    brinco_pai: string;
+    brinco_mae: string;
+    data_nascimento: Date;
+    data_saida: Date;
+    status: 'Ativo' | 'Vendido' | 'Morto';
+    sexo: 'M' | 'F';
 }
