be7c15d232627d2d62d963f588e53628e79a8fad
Ian Robert <iannrobert@hotmail.com>
2024-03-10 08:42:45 -0300

    Cadastro de Peso

--- src/app/components/cadastro-peso/cadastro-peso.component.ts
+++ src/app/components/cadastro-peso/cadastro-peso.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro-peso.component.css'
 })
 export class CadastroPesoComponent {
+  formCadastro: FormGroup;
+  suinos: any[];
 
-}
+  constructor(private formBuilder: FormBuilder, private suinoService: SuinoService) {
+    this.formCadastro = this.formBuilder.group({
+      brinco: ['', Validators.required],
+      data: ['', Validators.required],
+      peso: ['', Validators.required]
+    });
+
+    this.suinoService.suinos$.subscribe(suinos => {
+      this.suinos = suinos;
+    });
+  }
+
+    cadastrarPeso(): void {
+      if (this.formCadastro.valid) {
+        console.log(this.formCadastro.value);
+        // Aqui você pode enviar os dados para o backend
+        this.formCadastro.reset();
+      } else {
+        alert('Por favor, preencha todos os campos corretamente.');
+      }
+    }
+  }
+
