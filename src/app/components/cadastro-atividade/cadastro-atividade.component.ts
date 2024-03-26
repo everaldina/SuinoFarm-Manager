18a951f315de8e44557d4b64d8e2a7c42ca1cbe9
Caua-Clemente <cauaclementelima@gmail.com>
2024-03-25 21:41:01 -0300

    Implementando componente de cadastro de atividade

--- src/app/components/cadastro-atividade/cadastro-atividade.component.ts
+++ src/app/components/cadastro-atividade/cadastro-atividade.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DatabaseService } from '../../services/database.service';
+import { Atividade } from '../../models/atividade';
 
 @Component({
   selector: 'app-cadastro-atividade',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro-atividade.component.css'
 })
 export class CadastroAtividadeComponent {
+
+  formularioAtividade : FormGroup;
+
+  constructor(private database: DatabaseService) {
+    this.formularioAtividade = new FormGroup({
+      'descricao': new FormControl(null, [
+          Validators.required
+      ])
+    })
+  }
+
+  onSubmit(){
+    if(this.formularioAtividade.valid){
+      let resposta = this.formularioAtividade.value;
+      let atividade: Atividade = {
+        id: '',
+        descricao: resposta.descricao
+      }
+      console.log("Formulário válido", atividade);
+
+      this.database.addAtividade(atividade)
+      console.log(this.database.getAtividades());
+
+      this.formularioAtividade.reset();
+    }
+    else{
+      console.log("Formulário inválido");
+    }
+  }
 
 }
